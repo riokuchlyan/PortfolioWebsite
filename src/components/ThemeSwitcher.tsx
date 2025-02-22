c7dfb06ae575ecb5f1730a05d95bb1490a2b4453
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-02-21 23:22:49 -0500

    Refactor ThemeSwitcher component to use 'let' for color_scheme variable

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from "next/image";
 
-var color_scheme = "dark";
+let color_scheme = "dark";
 const ThemeSwitcher: React.FC = () => {
   const changeTheme = () => {
     if (color_scheme == "dark") {
