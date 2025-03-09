06f9e521dc3d1e1200a47ade4dd3f343ea64b7ee
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-03-09 16:10:25 -0400

    Edit ThemeSwitcher.tsx

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -7,22 +7,38 @@ const ThemeSwitcher: React.FC = () => {
     if (color_scheme == "dark") {
         color_scheme = "light";
         const root = document.documentElement;
-        const images = document.querySelectorAll('img');
         root.style.setProperty('--background', 'rgb(199, 199, 199)');
         root.style.setProperty('--foreground', '#1a1a1a');
-        images.forEach((img) => {
-          img.style.filter = "brightness(0%)";
-      });
+        const sun = document.getElementById('sun');
+        if (sun) {
+          sun.style.filter = "brightness(0%)";
+        }
+        const house = document.getElementById('house');
+        if (house) {
+          house.style.filter = "brightness(0%)";
+        }
+        const bird = document.getElementById('bird');
+        if (bird) {
+          bird.style.filter = "brightness(0%)";
+        }
     }
     else {
         color_scheme = "dark";
         const root = document.documentElement;
-        const images = document.querySelectorAll('img');
         root.style.setProperty('--background', '#1a1a1a');
         root.style.setProperty('--foreground', 'rgb(199, 199, 199)');
-        images.forEach((img) => {
-          img.style.filter = "brightness(100%)";
-      });
+        const sun = document.getElementById('sun');
+        if (sun) {
+          sun.style.filter = "brightness(100%)";
+        }
+        const house = document.getElementById('house');
+        if (house) {
+          house.style.filter = "brightness(100%)";
+        }
+        const bird = document.getElementById('bird');
+        if (bird) {
+          bird.style.filter = "brightness(100%)";
+        }
     }
   };
 
