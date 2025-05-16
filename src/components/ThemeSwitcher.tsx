855dd493fa0dcfe2c07ae03ff0e5e76b0c0065de
Rio Kuchlyan <riokuchlyan@gmail.com>
2025-05-15 23:46:22 -0400

    Remove unused theme validation import from ThemeSwitcher component

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from "next/image";
 import { getColorScheme, setColorScheme } from '../utils/theme';
-import { validateTheme } from "@/utils/validateTheme";
 
 const ThemeSwitcher: React.FC = () => {
   const changeTheme = () => {
